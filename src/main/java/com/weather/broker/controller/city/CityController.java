167e2f344fe0e92a5f210c761e5aba359198ef8b
Smartrb111 <wilwis@mail.ru>
2023-01-14 13:48:19 +0300

    first commit

--- src/main/java/com/weather/broker/controller/city/CityController.java
+++ src/main/java/com/weather/broker/controller/city/CityController.java
@@ -1,19 +1,29 @@
 package com.weather.broker.controller.city;
 
 import com.weather.broker.dto.CityDto;
+import com.weather.broker.entity.CityEntity;
 import com.weather.broker.service.CityService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("/citiy")
 @AllArgsConstructor
 public class CityController {
+
     private final CityService cityService;
+
+    private final CityDto cityDto;
+
 
     @GetMapping
     public CityDto getCityById(@RequestParam Long cityId) throws Exception {
-        return cityService.getCity(cityId);
+        return cityService.getById(cityId);
+    }
+
+    @PostMapping("/cities")
+    public ResponseEntity<CityEntity> createCity(@RequestBody CityDto сityDto) {
+
+        return ResponseEntity.ok(cityService.create(cityDto));
     }
 }
--- src/main/resources/db/migration/V1_Init_DB.sql
+++ src/main/resources/db/migration/V1_Init_DB.sql
@@ -1,6 +1,8 @@
 create table city
 (
-    id             bigserial PRIMARY KEY not null,
-    name           varchar(),
-    current_weather varchar()
+    id      bigserial PRIMARY KEY not null,
+    name    VARCHAR(512),
+    state   VARCHAR(512),
+    country VARCHAR(512),
+    coord   VARCHAR(512)
 );
